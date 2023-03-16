cf0e7fdac22e8d2ff9bd4f855cca3e307e9fb560
MazenTej <68936353+MazenTej@users.noreply.github.com>
2023-03-16 19:43:12 -0400

    Update Dashboard.js

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import {
   useSignOut,
   useViewSelector,
 } from "react-use-analytics-api";
-// react-bootstrap components
+//react-bootstrap components
 import { Card, Container, Row, Col, Nav } from "react-bootstrap";
 import "./style.css";
 import { useSelector } from "react-redux";
@@ -16,80 +16,80 @@ function Dashboard() {
   const view = useSelector((state) => state.view);
   console.log("viewww", view);
 
-  // const [viewId, setViewId] = useState();
+   const [viewId, setViewId] = useState();
 
-  // useEffect(() => {
-  //   setViewId(view);
-  // }, [view]);
+   useEffect(() => {
+     setViewId(view);
+   }, [view]);
 
-  // const viewSelectorContainerId1 = "view-selector-container1";
-  // const viewSelectorContainerId2 = "view-selector-container2";
+   const viewSelectorContainerId1 = "view-selector-container1";
+   const viewSelectorContainerId2 = "view-selector-container2";
 
-  // useViewSelector(
-  //   authorized ? gapi : undefined,
-  //   viewSelectorContainerId1,
-  //   (viewId) => setViewId(viewId)
-  // );
-  // useViewSelector(
-  //   authorized ? gapi : undefined,
-  //   viewSelectorContainerId2,
-  //   (viewId) => setViewId(viewId)
-  // );
+   useViewSelector(
+     authorized ? gapi : undefined,
+   viewSelectorContainerId1,
+     (viewId) => setViewId(viewId)
+   );
+   useViewSelector(
+     authorized ? gapi : undefined,
+     viewSelectorContainerId2,
+     (viewId) => setViewId(viewId)
+   );
 
-  // const query = {
-  //   ids: viewId,
-  //   "start-date": "30daysAgo",
-  //   "end-date": "today",
-  //   metrics: "ga:sessions",
-  //   dimensions: "ga:date",
-  // };
-  // const chart = {
-  //   container: "data-chart-container1",
-  //   type: "LINE",
-  //   options: {
-  //     title: "Sessions (20 Days)",
-  //   },
-  // };
-  // const query2 = {
-  //   ids: viewId,
-  //   "start-date": "30daysAgo",
-  //   "end-date": "today",
-  //   metrics: "ga:users",
-  //   dimensions: "ga:userType",
-  // };
+   const query = {
+     ids: viewId,
+     "start-date": "30daysAgo",
+     "end-date": "today",
+     metrics: "ga:sessions",
+     dimensions: "ga:date",
+   };
+   const chart = {
+     container: "data-chart-container1",
+     type: "LINE",
+     options: {
+      title: "Sessions (20 Days)",
+     },
+   };
+   const query2 = {
+     ids: viewId,
+     "start-date": "30daysAgo",
+     "end-date": "today",
+     metrics: "ga:users",
+     dimensions: "ga:userType",
+   };
 
-  // const chart2 = {
-  //   container: "data-chart-container2",
-  //   type: "PIE",
-  //   options: {
-  //     title: "Users (30 Days)",
-  //   },
-  // };
+   const chart2 = {
+     container: "data-chart-container2",
+     type: "PIE",
+     options: {
+       title: "Users (30 Days)",
+     },
+   };
 
-  // useDataChart(authorized ? gapi : undefined, query, chart);
-  // useDataChart(authorized ? gapi : undefined, query2, chart2);
+   useDataChart(authorized ? gapi : undefined, query, chart);
+   useDataChart(authorized ? gapi : undefined, query2, chart2);
 
-  // const authDiv = useRef(null);
-  // const [authorizeCalled, setAuthorizeCalled] = useState(false);
-  // const hasAuthElements =
-  //   authDiv.current && authDiv?.current?.children?.length > 0;
+   const authDiv = useRef(null);
+   const [authorizeCalled, setAuthorizeCalled] = useState(false);
+   const hasAuthElements =
+     authDiv.current && authDiv?.current?.children?.length > 0;
 
-  // const authorize = useAuthorize(gapi, {
-  //   clientId:
-  //     "256529246232-m2jn6rivk1vvsv8cuo692m0dqn4ukc6k.apps.googleusercontent.com",
-  //   container: "authorize-container-id",
-  // });
+   const authorize = useAuthorize(gapi, {
+     clientId:
+       "256529246232-m2jn6rivk1vvsv8cuo692m0dqn4ukc6k.apps.googleusercontent.com",
+     container: "authorize-container-id",
+   });
 
-  // useEffect(() => {
-  //   if (ready && !error && !authorizeCalled) {
-  //     authorize();
-  //     setAuthorizeCalled(true);
-  //   }
-  // }, [ready, error, authorizeCalled, authorize]);
+   useEffect(() => {
+     if (ready && !error && !authorizeCalled) {
+       authorize();
+       setAuthorizeCalled(true);
+     }
+   }, [ready, error, authorizeCalled, authorize]);
 
   return (
     <div id="page-content-wrapper">
-      {/* <div className="container-fluid">
+       <div className="container-fluid">
         <Row>
           <Col md="auto">
             <Card style={{ width: "40rem" }}>
@@ -101,8 +101,8 @@ function Dashboard() {
                 <div
                   style={{ height: "75vh" }}
                   className="container valign-wrapper"
-                > */}
-      {/* <div className="row">
+                > 
+       <div className="row">
                     <div className="col s12 center-align">
                       <div>
                         {!ready && <div>Loading...</div>}
@@ -114,12 +114,12 @@ function Dashboard() {
                                   <div
                                     className="data-chart"
                                     id="data-chart-container1"
-                                  /> */}
-      {/* <div
+                                  /> 
+     <div
                                     id={viewSelectorContainerId1}
                                     className="data-chart"
-                                  /> */}
-      {/* </div>
+                                  /> 
+       </div>
                               </div>
                             )}
                             {!authorized && (
@@ -139,7 +139,7 @@ function Dashboard() {
                       </div>
                     </div>
                   </div> */}
-      {/* </div>
+      </div>
               </Card.Body>
               <Card.Footer>
                 <div className="legend">
@@ -155,8 +155,8 @@ function Dashboard() {
                 </div>
               </Card.Footer>
             </Card>
-          </Col> */}
-      {/* <Col md="auto">
+          </Col> 
+      <Col md="auto">
             <Card style={{ width: "35rem" }}>
               <Card.Header>
                 <Card.Title as="h4">Users Statistics</Card.Title>
@@ -164,12 +164,12 @@ function Dashboard() {
               </Card.Header>
               <Card.Body>
                 <div className="data-chart" id="data-chart-container2" />
-                {/* <div id={viewSelectorContainerId2} /> */}
-      {/* </Card.Body>
+                {/* <div id={viewSelectorContainerId2} /> 
+       </Card.Body>
             </Card>
           </Col>
         </Row>
-      </div> */}
+      </div> 
     </div>
   );
 }
